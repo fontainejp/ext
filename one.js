a289dd3a4ee38fcfb9fe36aaa8e8cbbe90c5a361
Fontaine Jean Philippe <fontaine.jp@gmail.com>
2023-10-08 10:12:48 +0200

    Create one.js

---
+++ one.js
@@ -0,0 +1,16 @@
+// Mes blocs
+
+var mes_blocs = ['pi']
+
+////////////////////
+Blockly.Blocks['pi']={init:function(){
+    this.appendDummyInput()
+        .appendField("3.14");
+    this.setInputsInline(false);
+    this.setOutput(true, "Number");
+    this.setColour('#00CC00')}
+}
+Blockly.Arduino['pi']=function(block){
+    return ["3.14", Blockly.Arduino.ORDER_ATOMIC];
+}
+////////////////////
